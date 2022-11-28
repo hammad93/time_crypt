268c29eee69d024523668b5e9f7c658518b67c35
Hammad Usmani <ham.u@live.com>
2022-11-28 16:25:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ def encrypt(message):
     Encrypts the message using the global public key
     '''
     message = pgpy.PGPMessage.new(message)
-    encrypted_string = str(PGPKey.from_blob(PUBLIC_KEY)[0].encrypt(message), 'UTF-8')
+    encrypted_string = str(PGPKey.from_blob(PUBLIC_KEY)[0].encrypt(message))
     print(encrypted_string)
     return encrypted_string
 
